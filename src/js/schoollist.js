92646de2353af729f1116ff1f340269d997e32f4
jiabinleo@126.com <jiabinleo@126.com>
2022-05-27 05:38:25 +0000

    修改界面结构

--- src/js/schoollist.js
+++ src/js/schoollist.js
@@ -166,7 +166,7 @@ const schoollist = {
                     }
                 },
                 error(err){
-                    let li = `<li><div class="top"><a href="javascript:"><div class="schoollog"><img src="https://www.ieduchina.com/uploadfile/college/202012/1607653957.png" alt="北京师范大学附属实验中学国际部"></div><div class="price"><span>参考学费</span><p>12.8万~20.8万</p></div></a></div><div class="center"><a href="javascript:"><h3>上海高藤致远创新学校</h3><div class="text"><p>上海高藤致远创新学校位于浦东绿地国际教育园区，是与绿地香港股份有限公司（全球500强企业绿地集团控股子公司）战略合作上海高藤致远创新学校位于…</p></div></a><div class="tools"><a>2020人关注</a> <a>2020人咨询</a> <a>预约探校</a></div></div><div class="bottom"><a href="javascript:"><div class="image" style="background-image: url(https://www.ieduchina.com/uploadfile/202205/f46120220520145636.jpg);"></div><div class="text"><div class="title"><h4>深扒包玉刚：中国排名第一！据说马伊琍女儿就读该校！它凭什么这么牛？</h4></div><p>上海包玉刚实验学校</p></div></a></div></li>`
+                    let li = `<li><div class="top"><a href="javascript:"><div class="schoollog"><img src="https://www.ieduchina.com/uploadfile/college/202012/1607653957.png" alt="北京师范大学附属实验中学国际部"></div><div class="price"><span>参考学费</span><p>12.8万~20.8万</p></div></a></div><div class="center"><a href="javascript:"><h3>上海高藤致远创新学校</h3><div class="text"><p>上海高藤致远创新学校位于浦东绿地国际教育园区，是与绿地香港股份有限公司（全球500强企业绿地集团控股子公司）战略合作上海高藤致远创新学校位于…</p></div></a><div class="tools"><a>2020人关注</a> <a>2020人咨询</a> <a>预约探校</a></div></div><div class="bottom"><a href="javascript:"><div class="image" style="background-image: url(https://www.ieduchina.com/uploadfile/202205/f46120220520145636.jpg);"></div><div class="text"><div class="title"><h4>深扒包玉刚：中国排名第一！据说马伊琍女儿就读该校！它凭什么这么牛？</h4></div></div></a></div></li>`
                     ul.append(li);
                 }
             })
