262769563cc35941d771388874854561a6ae239b
jiabinleo <jiabinleo@126.com>
2022-12-30 10:49:14 +0800

    元旦皮肤

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -4,7 +4,7 @@ import "@cs/header";
 import "@cs/footer"
 import "./components/banner/index.ts";
 import "./components/menu/index.ts";
-import "@/skin/2022圣诞节";
+import "@/skin/2023元旦";
 /*webpackjs*/
 
 /*webpackjs*/
